62dcb92343e9714911751aaa07420589b65e7739
james.kim <james.kim@ticketmaster.com>
2020-11-05 13:19:49 -0800

    refactor - space page to space view

--- views/Space/MyPosts.js
+++ views/Space/MyPosts.js
@@ -1,4 +1,4 @@
-import Card from '../Card'
+import Card from '../../components/Card'
 import NoPosts from './NoPosts'
 import {
     MyPostsContainer
--- views/Space/styles.js
+++ views/Space/styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
+export const MyPostsContainer = styled.section`
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10rem;
+`
+
+export const NoPostsContainer = styled.section`
+    background: #DDDDDD;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 1rem;
+    padding: 2rem 0;
+
+    img {
+        width: 45%;
+    }
+
+    h2{
+        color: #5f5f5f;
+    }
+`
+
 export const SpaceMenuContainer = styled.section`
     background-color: #f1f1f1;
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
