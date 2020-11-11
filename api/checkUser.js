c3279c75a5aeffd61f65edfe664aa15799e29a01
james.kim <james.kim@ticketmaster.com>
2020-11-11 10:56:25 -0800

    refactor - add a default state on userCheck

--- api/checkUser.js
+++ api/checkUser.js
@@ -2,7 +2,12 @@ import { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 
 const checkUser = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({
+    attributes : {
+      sub: null,
+    },
+    username : null
+  })
   const [loading, setLoading] = useState(false)
 
   useEffect(async () => {
